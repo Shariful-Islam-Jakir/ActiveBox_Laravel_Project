04c40f931b4747c31d7e87c92171b40468ab21f5
Md Shariful Islam Jakir <jakirhabib88@gmail.com>
2026-01-31 11:08:49 +0600

    new file working

--- resources/views/Home.blade.php
+++ resources/views/Home.blade.php
@@ -8,18 +8,22 @@
     <!--Bootstrap-->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
     <!--Main-CSS-->
+
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <link rel="stylesheet" href="css/responsive.css">
+    
   </head>
   <body>
     
     <header class="ActiveBox_header">
         <div class="container">
-            <div class="row">
-                <div class="col-lg-2">
+            <div class="row align-items-center">
+                <div class="col-lg-2 col-4">
                     <div class="ActiveBox_logo">
-                        <a href=""><img src=""></a>
+                        <a href=""><img src="{{ asset('image/ActiveBox logo.png') }}"></a>
                     </div>
                 </div>
-                <div class="col-lg-10">
+                <div class="col-lg-10 col-8">
                     <nav class="ActiveBox_Menu">
                         <ul>
                             <li><a href="">Home</a></li>
@@ -30,13 +34,222 @@
                             <li><a href="">Download</a></li>
                         </ul>
                     </nav>
+                    <div class="ActiveBox_Menu_Icon">
+                         <i class="fa-solid fa-bars"></i>
+                    </div>
                 </div>
             </div>
         </div>
     </header>
 
+    <!--Header End-->
+
+    <!--Banner area-->
+
+    <div class="ActiveBox_banner">
+        <div class="container">
+            <div class="ActiveBox_content">
+                <div>
+                    <h1>Active Box</h1>
+                    <p>Lorem color,Lorem Font,Lorem Background</p>
+                </div>
+                <div>
+                    <button>Call Now</button>
+                    <a href="#"><i class="fa-solid fa-chevron-down"></i></a>
+                </div>
+                
+            </div>
+        </div>
+    </div>
+
+    <!--Banner End-->
+
+    <div class="ActiveBox_About">
+        <div class="container">
+            <div class="ActiveBox_tittle">
+                <h1>Active Box About</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id ut dolore repudiandae, inventore exercitationem modi aut delectus. Esse omnis unde natus, fugiat hic accusamus provident! Doloremque aperiam consequuntur itaque labore?</p>
+            </div>
+        </div>
+    </div>
+
+    <!--ActiveBox About End-->
+
+    <div class="ActiveBox_Service">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-4">
+                    <div class="ActiveBox_Service_Icon">
+                        <i class="fa-solid fa-mobile-screen-button"></i>
+                    </div>
+                    <div class="ActiveBox_Service_Tittle">
+                        <h5>Live Phone</h5>
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <div class="ActiveBox_Service_Icon">
+                        <i class="fa-solid fa-mobile-screen-button"></i>
+                    </div>
+                    <div class="ActiveBox_Service_Tittle">
+                        <h5>Live Phone</h5>
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <div class="ActiveBox_Service_Icon">
+                        <i class="fa-solid fa-mobile-screen-button"></i>
+                    </div>
+                    <div class="ActiveBox_Service_Tittle">
+                        <h5>Live Phone</h5>
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!--Service end-->
+
+    <div class="ActiveBox_Work">
+        <div class="container">
+            <div class="ActiveBox_Work_Tittle">
+                <h2>Active Box Work Tittle</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor architecto quos sit impedit nemo nobis dolorum saepe est ipsam velit odit omnis blanditiis officia,.</p>
+            </div>
+
+            <div class="row">
+                <div class="col-lg-4">
+                    <div class="ActiveBox_Work_Image">
+                        <img src="image/image eight.png">
+                        <div class="ActiveBox_Work_Image_hover">
+                            <ul>
+                                <li><i class="fa-brands fa-facebook"></i></li>
+                                <li><i class="fa-brands fa-instagram"></i></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <div class="ActiveBox_Work_Image">
+                        <img src="image/image eight.png">
+                        <div class="ActiveBox_Work_Image_hover">
+                            <ul>
+                                <li><i class="fa-brands fa-facebook"></i></li>
+                                <li><i class="fa-brands fa-instagram"></i></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <div class="ActiveBox_Work_Image">
+                        <img src="image/image eight.png">
+                        <div class="ActiveBox_Work_Image_hover">
+                            <ul>
+                                <li><i class="fa-brands fa-facebook"></i></li>
+                                <li><i class="fa-brands fa-instagram"></i></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <div class="ActiveBox_Work_Image">
+                        <img src="image/image eight.png">
+                        <div class="ActiveBox_Work_Image_hover">
+                            <ul>
+                                <li><i class="fa-brands fa-facebook"></i></li>
+                                <li><i class="fa-brands fa-instagram"></i></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <div class="ActiveBox_Work_Image">
+                        <img src="image/image eight.png">
+                        <div class="ActiveBox_Work_Image_hover">
+                            <ul>
+                                <li><i class="fa-brands fa-facebook"></i></li>
+                                <li><i class="fa-brands fa-instagram"></i></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <div class="ActiveBox_Work_Image">
+                        <img src="image/image eight.png">
+                        <div class="ActiveBox_Work_Image_hover">
+                            <ul>
+                                <li><i class="fa-brands fa-facebook"></i></li>
+                                <li><i class="fa-brands fa-instagram"></i></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!--Active Box Work End-->
+
+    <div class="ActiveBox_Contact">
+        <div class="container">
+             <div class="ActiveBox_Contact_Tittle">
+                <h2>Active Box Contact</h2>
+                <p>Lorem ipsum dolor sit officia,.</p>
+            </div>
+            <div class="row">
+                <div class="ActiveBox_From">
+                    <div class="ActiveBox_Input">
+                        <input type="text" name="name" placeholder="enter your name" class="form-control">
+                    </div>
+                    <div class="ActiveBox_Input">
+                        <input type="email" name="email" placeholder="enter your email" class="form-control">
+                    </div>
+                    <div class="ActiveBox_Input">
+                        <textarea cols="30" rows="7" placeholder="enter your message" class="form-control" ></textarea>
+                    </div>
+
+                    <div class="ActiveBox_foot">
+                        <div class="ActiveBox_foot_left">
+                            <input type="checkbox" name="sub" id="sub">
+                            <label>Subscribe Check Box</label>
+                        </div>
+                        <div class="ActiveBox_foot_right">
+                            <input type="submit" value="send">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!--Active Box Contact End-->
+
+    <!--Footer Section-->
+
+    <div class="Footer_area">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-4">
+                    <div class="ActiveBox_Footer_Tittle">
+                        <p>@ Copy Right 2026</p>
+                    </div>
+                </div>
+                <div class="col-lg-8">
+                    <div class="ActiveBox_Footer_Icon">
+                        <i class="fa-brands fa-facebook"></i>
+                        <i class="fa-brands fa-instagram"></i>
+                        <i class="fa-brands fa-youtube"></i>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
     <!--bootstrap-->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+
+    <!---Fontawesome--->
+    <script src="https://kit.fontawesome.com/c9c243d52a.js" crossorigin="anonymous"></script>
   </body>
 </html>
